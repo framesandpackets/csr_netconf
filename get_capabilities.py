9f347a4971df7960ef2cf368e618c6b40c4bbc6d
FramesandPackets <michaelhanna272@hotmail.co.uk>
2020-12-15 20:41:32 +0000

    comments added

--- get_capabilities.py
+++ get_capabilities.py
@@ -1,14 +1,18 @@
 from ncclient import manager
 
-csr_manager = manager.connect(host='192.168.0.150',
+#calling manager.connect from ncclient module to connect to Cisco CSR1000v
+#you need enter the specific values of your CSR device here
+csr_manager = manager.connect(host='hostname/ip',
                               port=830,
-                              username='michael',
-                              password='pass1',
+                              username='username',
+                              password='password',
                               hostkey_verify=False,
                               device_params={'name':'csr'}
                               )
 
+#for loop to print the list of capabilities of the Cisco CSR1000v
 for csr_capability in csr_manager.server_capabilities:
     print(csr_capability)
 
+#closing netconf session
 csr_manager.close_session()
